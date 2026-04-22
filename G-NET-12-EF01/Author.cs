68cca2aec04cef8f6b4ccb66265a727be9b09693
marco <marco@DESKTOP-GCUIOA1>
2026-04-22 21:49:16 +0200

    Class Author

---
+++ G-NET-12-EF01/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G_NET_12_EF01
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Bio { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
