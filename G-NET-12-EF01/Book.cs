37a7354dbb43288166b2889608b7077c75d7dce4
marco <marco@DESKTOP-GCUIOA1>
2026-04-22 21:46:06 +0200

    Class Books

---
+++ G-NET-12-EF01/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G_NET_12_EF01
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public decimal Price { get; set; }
+        public int NumberOfPages { get; set; }
+        public int YearPublished { get; set; }
+        public bool IsInStock { get; set; }
+    }
+}
