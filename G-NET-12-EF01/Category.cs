6b8f974613a4b44a55c7189595130a3ef3b6ab9b
marco <marco@DESKTOP-GCUIOA1>
2026-04-22 21:50:26 +0200

    Class Category

---
+++ G-NET-12-EF01/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G_NET_12_EF01
+{
+    public  class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
